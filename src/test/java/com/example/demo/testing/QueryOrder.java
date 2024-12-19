88a931f233091f171da3f7b9db405b6ba9571a6e
KuanTa <KuanTa@LinKuanTa>
2024-12-19 13:42:26 +0800

    PSI

--- src/test/java/com/example/demo/testing/QueryOrder.java
+++ src/test/java/com/example/demo/testing/QueryOrder.java
@@ -19,6 +19,35 @@ public class QueryOrder {
 	public void test() {
 		List<Order> orders = orderRepository.findAll();
 		
-		
+		orders.forEach(order -> {
+			// 該筆訂單總價
+			int total = order.getOrderItems()
+					         .stream()
+					         .mapToInt(item -> 
+					        	 item.getAmount() * item.getProduct().getPrice())
+					         .sum();
+			System.out.printf("訂單編號:%d ,客戶:%s ,員工:%s, 總價:%d\n",
+					order.getId(),
+					order.getCustomer().getName(),
+					order.getEmployee().getName(),
+					total);
+			System.out.println("訂單明細:");
+			order.getOrderItems().forEach(item -> {
+				System.out.printf("\t明細編號:%d ,商品:%s ,數量:%d ,單價:%d ,小計:%d\n",
+						item.getId(),
+						item.getProduct().getName(),
+						item.getAmount(),
+						item.getProduct().getPrice(),
+						(item.getProduct().getPrice())*(item.getAmount()));
+			});
+			System.out.println("-------------------------------------------");
+		});
+		int sumTotal = orders.stream()
+				             .mapToInt(order -> order.getOrderItems()
+                                                     .stream()
+                                                     .mapToInt(item -> item.getProduct().getPrice() * item.getAmount())
+                                                     .sum())
+				             .sum();               
+		System.out.printf("所有訂單的總價:%d",sumTotal);
 	}
 }
